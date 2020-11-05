88411c06d2417238ea9b9856b46d353ec3e0d967
Khairan <alphakhairan@gmail.com>
2020-11-05 20:46:16 +0300

    Сделал урок № 3

---
+++ Lesson3/Scripts/MyEnemy.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+
+namespace Hosthell
+{
+    public sealed class MyEnemy : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private float _speed = 1.0f;
+        [SerializeField] private int _health = 10;
+
+        private GameObject _target;
+
+        #endregion
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _target = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            LookAtPlayer();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void LookAtPlayer()
+        {
+            transform.LookAt(_target.transform);
+        }
+
+        public void Hurt(int damage)
+        {
+            _health -= damage; ;
+
+            if (_health <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
---
+++ Lesson3/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+namespace Hosthell
+{
+    public sealed class PlayerMovement : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private float _speed = 5.0f;
+
+        private Vector3 _moveDirection;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Update()
+        {
+            Move();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void Move()
+        {
+            _moveDirection.x = Input.GetAxis("Horizontal");
+            _moveDirection.z = Input.GetAxis("Vertical");
+                        
+            var movementDirection = _moveDirection * _speed * Time.deltaTime;
+
+            transform.LookAt(movementDirection + transform.position);
+            transform.position += movementDirection;
+        }
+
+        #endregion
+    }
+}
