be7a549115584677b3eaff21b3eab668f6d68d3e
Khairan <alphakhairan@gmail.com>
2020-11-25 13:58:46 +0300

    Сделал 7 урок

---
+++ Lesson7/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Hosthell
+{
+    public sealed class MenuController : MonoBehaviour
+    {
+        #region UnityMethods
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void LoadLevel(string levelName)
+        {
+            SceneManager.LoadScene(levelName);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        #endregion
+    }
+}
