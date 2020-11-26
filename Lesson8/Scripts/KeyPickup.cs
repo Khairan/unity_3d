b560aff42898a8b094c883535fdbd33659136f2e
Khairan <alphakhairan@gmail.com>
2020-11-26 20:52:40 +0300

    Финальный проект

---
+++ Lesson8/Scripts/KeyPickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Hosthell
+{
+    public sealed class KeyPickup : MonoBehaviour
+    {
+        #region UnityMethods
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                var player = other.GetComponent<PlayerController>();
+                player.GoldenKey = true;
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
